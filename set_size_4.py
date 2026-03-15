ae1b5bd500abce70425081d3ecc048f4377c5eb4
John Nestor <jnestor@rogers.com>
2026-03-14 20:52:01 -0400

    Fix cursor size scaling on Windows 11 using undocumented SPI_SETCURSORSIZE API

--- set_size_4.py
+++ set_size_4.py
@@ -18,3 +18,8 @@ SPI_SETCURSORS = 0x0057
 SPIF_UPDATEINIFILE = 0x01
 SPIF_SENDCHANGE = 0x02
 ctypes.windll.user32.SystemParametersInfoW(SPI_SETCURSORS, 0, None, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE)
+
+# Windows 10/11 Accessibility Cursor Scale (Undocumented API)
+# This is explicitly required on modern Windows to scale the cursor visually
+SPI_SETCURSORSIZE = 0x2029
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETCURSORSIZE, 0, 53, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE)
