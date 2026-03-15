b699841978be76327e525dfcb955407ae6aa6ce7
John Nestor <jnestor@rogers.com>
2026-03-14 20:44:06 -0400

    Initial commit

---
+++ set_size_1.py
@@ -0,0 +1,20 @@
+import winreg
+import ctypes
+
+# Update accessibility preference (slider position)
+key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Accessibility", 0, winreg.KEY_SET_VALUE)
+winreg.SetValueEx(key, "CursorSize", 0, winreg.REG_DWORD, 1)
+winreg.CloseKey(key)
+
+# Update the actual cursor base size that SPI_SETCURSORS reads
+# Windows maps size 1-15 to CursorBaseSize 32-128: round(32 + (size-1)*96/14)
+# Size 1 -> 32
+key2 = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Control Panel\Cursors", 0, winreg.KEY_SET_VALUE)
+winreg.SetValueEx(key2, "CursorBaseSize", 0, winreg.REG_DWORD, 32)
+winreg.CloseKey(key2)
+
+# Reload cursors and broadcast the change to all windows
+SPI_SETCURSORS = 0x0057
+SPIF_UPDATEINIFILE = 0x01
+SPIF_SENDCHANGE = 0x02
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETCURSORS, 0, None, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE)
